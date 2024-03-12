282c5752f0758f814927caddcbe6b30b66282617
Julianne Duhaze <jduhaze@protonmail.com>
2024-03-12 10:10:38 +0100

    style: replace default css file by sass file

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Replace the default required index.css by main.scss file
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/style/main.scss";`
+      },
+    },
+  },
 })
