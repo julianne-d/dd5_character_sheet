f71a30edaae391cc48f1633cefaf4bc81c986ede
Julianne Duhaze <jduhaze@protonmail.com>
2024-03-13 11:14:21 +0100

    feat: created a new character form page

---
+++ src/pages/Form/CharacterForm.page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import IdCard from '../../components/IdCard/IdCard.component'
+
+export default function FormPage() {
+    return (
+      <>
+        <form action="" method="get" className="form-example">
+            <IdCard />
+        </form>
+      </>
+    )
+}
